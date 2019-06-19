8c32c33af9a7b91e3a5aa1019bdcb36782896fca
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-06-19 15:42:45 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainViewComponent } from './main-view/main-view.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { StartComponent } from './start/start.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainViewComponent,
+    QuestionsComponent,
+    StartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
