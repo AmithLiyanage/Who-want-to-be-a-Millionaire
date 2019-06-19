19f4b9afb0a091bd6454ea80cc910b5aaa818b59
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-06-19 21:11:46 +0530

    start to end work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StartComponent } from './start/start.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { MainViewComponent } from './main-view/main-view.component';
 
 const routes: Routes = [
   {path: 'start', component: StartComponent},
+  {path: '', component: MainViewComponent},
   {path: 'question/:id', component: QuestionsComponent}
 ];
 
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -9,43 +9,44 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class QuestionsComponent implements OnInit {
 
-  isCorrected:boolean;
-  question:{};
-  id:string;
+  isCorrected: boolean;
+  question: any;
+  id: string;
   answerClasses = {
     1: '',
     2: '',
     3: '',
     4: ''
-  }
+  };
 
-  constructor(private qService:QuestionsService, private router:ActivatedRoute, private route: Router) { }
+  constructor(private qService:QuestionsService, private router: ActivatedRoute, private route: Router) { }
 
   ngOnInit() {
     this.id = this.router.snapshot.paramMap.get('id');
     // console.log("id"+this.id)
-    this.question= this.qService.getQuestion(parseInt(this.id));
-    console.log(this.question)
+    // tslint:disable-next-line: radix
+    this.question = this.qService.getQuestion(parseInt(this.id));
+    console.log(this.question);
   }
 
-  checkAnswer(id:number){
-    if(this.question.correct == id){
-      this.isCorrected = true
-      this.answerClasses[id]="btnGreen";
-      console.log('correct answer')
-    }else{
+  checkAnswer(id: number){
+    if (this.question.correct == id) {
+      this.isCorrected = true;
+      this.answerClasses[id] = 'btnGreen';
+      console.log('correct answer');
+    } else {
       this.isCorrected = false;
-      this.answerClasses[id]="btnError";
-      this.answerClasses[this.question.correct]="btnGreen";
-      console.log('wrong answer')
+      this.answerClasses[id] = 'btnError';
+      this.answerClasses[this.question.correct] = 'btnGreen';
+      console.log('wrong answer');
     }
   }
 
-  gotonext(){
-    let nextquestionId = parseInt(this.id)+1;
-    if(this.isCorrected){
-      this.route.navigate(['/question/'+nextquestionId])
-    }else{
+  gotonext() {
+    const nextQuestionId = parseInt(this.id) + 1;
+    if (this.isCorrected) {
+      this.route.navigate(['/question/' + nextQuestionId]);
+    } else {
       this.route.navigate(['/question/1']);
     }
   }
